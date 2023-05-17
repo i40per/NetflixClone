7540b5ae4d34394cb4f1e0735f6e501de4b0c67b
Evgenii Lukin <l40perl@gmail.com>
2023-05-17 22:35:58 +0600

    Working with the second screen, added a poster, label and button.

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Евгений Лукин on 17.05.2023.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
